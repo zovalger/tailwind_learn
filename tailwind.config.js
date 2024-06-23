1924e245ec6e98ceba4ecca86b9fdac15af8b416
zovalger <zovalger@gmail.com>
2024-06-23 09:42:26 -0400

    nav bar responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./public/**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: { danger: "#f00" },
+			fontFamily: {
+				comic: ["Comic Neue"],
+			},
+		},
+	},
+	plugins: [],
+};
