cb88cfa7ec8b2359460a69fc41a5e6447819d63a
zovalger <zovalger@gmail.com>
2024-06-27 07:57:40 -0400

    fix bg image

--- src/components/SectionContainerWithImage.tsx
+++ src/components/SectionContainerWithImage.tsx
@@ -7,16 +7,8 @@ interface props {
 
 const SectionContainerWithImage = ({ imgURL, children }: props) => {
 	return (
-		<div className="relative min-h-screen h-fit min-w-screen  bg-red-500">
-			<div className="absolute top-0 bottom-0 left-0 right-0 inline-block ">
-				<img
-					src={imgURL || "https://picsum.photos/1200/1200"}
-					alt="sdsd"
-					className="object-cover w-full h-full"
-				/>
-			</div>
-
-			<div className="sticky bg-black bg-opacity-60 top-0 bottom-0 left-0 right-0 text-white px-5 py-14 text-balance">
+		<div className="relative min-h-screen h-fit min-w-screen flex flex-col  bg-[url(https://picsum.photos/1200/1200)] bg-cover bg-no-repeat">
+			<div className="bg-black bg-opacity-60 text-white px-5 py-14 text-balance h-full w-full inline-block flex-1">
 				{children}
 			</div>
 		</div>
